a1e16e7c240cee9976b6333fe883845775db061f
Guillermo Sequera <sequeracrespo@gmail.com>
2020-11-02 21:13:16 -0300

    project validation

--- servidor/routes/projects.js
+++ servidor/routes/projects.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
 const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 //crea proyectos
 //api/projects
 router.post('/', 
     auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
     proyectoController.crearProyecto
-)
+);
 
 router.get('/', 
     auth,
