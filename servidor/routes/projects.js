31b61f40e2402bcac53b1e8adc580d58e9347f0c
Guillermo Sequera <sequeracrespo@gmail.com>
2020-11-08 19:55:48 -0300

    update projects with PUT

--- servidor/routes/projects.js
+++ servidor/routes/projects.js
@@ -14,10 +14,19 @@ router.post('/',
     proyectoController.crearProyecto
 );
 
+//obtener todos los proyectos
 router.get('/', 
     auth,
     proyectoController.obtenerProyectos
 )
 
+//actualizar proyecto via ID
+router.put('/id',
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
+    proyectoController.actualizarProyecto
+);
 
 module.exports = router;
